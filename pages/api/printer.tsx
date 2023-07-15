f24331c9c409e428f0ca5987f82ba63de1063510
nabi.lee <hello@nabi.kim>
2023-07-16 00:50:58 +0900

    Use node-thermal-printer instead of react-thermal-printer

--- pages/api/printer.tsx
+++ pages/api/printer.tsx
@@ -1,84 +1,155 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Block, StyledText } from "@blocknote/core";
+import { Block, BlockSchema } from "@blocknote/core";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { connect } from "node:net";
 import {
-  render,
-  Printer,
-  Text,
-  Br,
-  Line,
-  Cut,
-  TextSize,
-} from "react-thermal-printer";
+  ThermalPrinter,
+  PrinterTypes,
+  CharacterSet,
+  breakLine,
+} from "node-thermal-printer";
 
-export default async function handler(
+export default async function handler<BSchema extends BlockSchema>(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const conn = connect({
-    host: process.env.PRINTER_HOST ?? "192.168.0.3",
-    port: Number(process.env.PRINTER_PORT ?? 9100),
-    timeout: 3000,
+  const printer = new ThermalPrinter({
+    type: PrinterTypes.EPSON,
+    interface: `tcp://${process.env.PRINTER_HOTS}:${process.env.PRINTER_PORT}`,
+    characterSet: CharacterSet.KOREA,
+    removeSpecialCharacters: false,
+    lineCharacter: "=",
+    options: {
+      timeout: 3000,
+    },
   });
 
-  if (conn.errored) {
-    res.status(500).json({ success: false, error: conn.errored });
-    return;
+  // const isConnected = await printer.isPrinterConnected();
+
+  // if (!isConnected)
+  //   return res.status(500).json({
+  //     error: true,
+  //     message: "프린터가 연결되어 있지 않습니다.",
+  //   });
+
+  function renderBlock(block: Block<BSchema>, depth = 0) {
+    // handle max depth
+    if (depth > 5)
+      return res.status(400).json({
+        error: true,
+        message: "max depth exceeded",
+      });
+
+    // render heading
+    if (block.type === "heading") {
+      const { level } = block.props;
+      const size = 4 - Number(level);
+      printer.setTextSize(size, size);
+    }
+
+    // render paragraph
+    if (block.type === "paragraph") {
+      printer.setTextSize(1, 1);
+    }
+
+    // render props
+    Object.entries(block.props).forEach(([key, value]) => {
+      // Align
+      if (key === "textAlignment") {
+        switch (value) {
+          case "left":
+            return printer.alignLeft();
+          case "center":
+            return printer.alignCenter();
+          case "right":
+            return printer.alignRight();
+        }
+      }
+
+      // Invert
+      if (key === "backgroundColor") {
+        if (value === "gray") {
+          printer.invert(true);
+        }
+        return;
+      }
+
+      // ignore props
+      if (key === "level") return;
+      if (key === "textColor") return;
+
+      res.status(400).json({
+        error: true,
+        message: "invalid prop",
+        prop: {
+          [key]: value,
+        },
+      });
+    });
+
+    // render contents
+    if (block.content.length > 0) {
+      renderContent(block.content, depth);
+    }
+
+    // render children
+    if (block.children.length > 0) {
+      block.children.forEach((child) => {
+        renderBlock(child, depth + 1);
+      });
+    }
   }
 
-  const data = await render(
-    <Printer width={42} type={"epson"} characterSet="korea">
-      {(req.body as Block<any>[]).map(({ type, content, props }) => {
-        console.log(type, content, props);
+  function renderContent(content: Block<BSchema>["content"], depth = 0) {
+    if (!content.length) return;
 
-        if (type === "heading")
-          return (
-            <Text
-              invert={props.backgroundColor === "default" ? true : undefined}
-              bold={(content[0] as StyledText)?.styles?.["bold"] ? true : false}
-              underline={
-                (content[0] as StyledText)?.styles?.["underline"]
-                  ? "1dot-thick"
-                  : "none"
-              }
-              align={props.textAlignment ?? "left"}
-              size={{
-                width: (4 - Number(props.level)) as TextSize,
-                height: (4 - Number(props.level)) as TextSize,
-              }}
-            >
-              {(content[0] as StyledText).text}
-            </Text>
-          );
-        if (type === "paragraph" && !content.length) return <Br {...props} />;
-        if (type === "paragraph")
-          return (
-            <Text
-              invert={props.backgroundColor === "gray" ? true : undefined}
-              bold={(content[0] as StyledText)?.styles?.["bold"] ? true : false}
-              underline={
-                (content[0] as StyledText)?.styles?.["underline"]
-                  ? "1dot-thick"
-                  : "none"
-              }
-              align={props.textAlignment ?? "left"}
-            >
-              {(content[0] as StyledText)?.text}
-            </Text>
-          );
-        if (type === "line") return <Line {...props} />;
-      })}
-      <Cut />
-    </Printer>
-  );
+    content.forEach((content) => {
+      if (content.type === "text") {
+        const { text } = content;
 
-  await new Promise<void>((resolve) => {
-    conn.write(data, () => {
-      conn.destroy();
-      resolve();
+        Object.entries(content.styles).forEach(([key, value]) => {
+          // Bold
+          if (key === "bold") {
+            console.debug("styles: bold");
+            if (value === true) printer.bold(true);
+            return;
+          }
+          // Underline
+          if (key === "underline") {
+            console.debug("styles: underline");
+            if (value === true) printer.underline(true);
+            return;
+          }
+
+          res.status(400).json({
+            error: true,
+            message: "unhandled style",
+            style: {
+              [key]: value,
+            },
+          });
+        });
+
+        return printer.println(String().padStart(depth * 2, " ") + text);
+      }
+
+      if (content.type === "link") {
+        return renderContent(content.content);
+      }
+
+      return res.status(400).json({
+        error: true,
+        message: "invalid content type",
+        content: content,
+      });
     });
-  });
+  }
 
-  res.status(200).json({ success: true });
+  req.body.forEach((item: Block<BSchema>) => renderBlock(item));
+
+  printer.cut();
+  await printer.execute();
+
+  res.status(200).end({
+    success: true,
+  });
 }
