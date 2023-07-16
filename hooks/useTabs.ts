04a08d42959e78e6757d51c23f3d01d761b89b40
nabi.lee <hello@nabi.kim>
2023-07-16 21:55:39 +0900

    Add tab feature

---
+++ hooks/useTabs.ts
@@ -0,0 +1,79 @@
+import { ChangeEvent, useState } from "react";
+
+export type Tab = {
+  title: string;
+};
+
+export function useTabs() {
+  const lastSelectedTab = globalThis.localStorage?.getItem("lastSelectedTab");
+  const initialTabList = globalThis.localStorage?.getItem("tabList");
+
+  const [tabList, setTabList] = useState<Tab[]>(
+    initialTabList ? JSON.parse(initialTabList) : [{ title: "무제" }]
+  );
+  const [currentTabIndex, setCurrentTabIndex] = useState(
+    lastSelectedTab ? Number(lastSelectedTab) : 0
+  );
+
+  const currentTab = tabList[currentTabIndex];
+
+  const setCurrentTab = (index: number) => {
+    globalThis?.localStorage.setItem("lastSelectedTab", String(index));
+    setCurrentTabIndex(index);
+  };
+
+  const newTab = () => {
+    setTabList((tabs) => {
+      const newTab = [...tabs, { title: "" }];
+      globalThis.localStorage.setItem("tabList", JSON.stringify(newTab));
+      return newTab;
+    });
+    setCurrentTab(tabList.length);
+  };
+
+  const renameTab =
+    (index: number) => (event: ChangeEvent<HTMLInputElement>) => {
+      setTabList((tabList) => {
+        const newTabList = [...tabList];
+        newTabList[index].title = event.target.value;
+
+        globalThis.localStorage.setItem("tabList", JSON.stringify(newTabList));
+        return newTabList;
+      });
+    };
+
+  const removeTab = (index: number) => () => {
+    setTabList((tabList) => {
+      const newTabList = [...tabList];
+      newTabList.splice(index, 1);
+
+      globalThis.localStorage.setItem("tabList", JSON.stringify(newTabList));
+      return newTabList;
+    });
+    setCurrentTab(currentTabIndex - 1 < 0 ? 0 : currentTabIndex - 1);
+  };
+
+  const setTab = (index: number) => () => {
+    setCurrentTab(index);
+  };
+
+  const isCurrentTab = (index: number) => {
+    return index === currentTabIndex;
+  };
+
+  if (!initialTabList) {
+    globalThis.localStorage?.setItem("tabList", JSON.stringify(tabList));
+  }
+
+  return {
+    tabList,
+    currentTab,
+    currentTabIndex,
+
+    newTab,
+    renameTab,
+    removeTab,
+    setTab,
+    isCurrentTab,
+  };
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,22 @@ import { CustomDragHandleMenu } from "@/blocknote/sideMenu";
 import { CustomFormattingToolbar } from "@/blocknote/toolbar";
 import { toast } from "react-toastify";
 import { useTheme } from "@/hooks/useTheme";
+import { FaPlus } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+import { useTabs } from "@/hooks/useTabs";
 
 export default function Home() {
+  const {
+    tabList: tabs,
+    currentTab,
+    currentTabIndex,
+
+    newTab,
+    renameTab,
+    removeTab,
+    setTab,
+    isCurrentTab,
+  } = useTabs();
   const [contents, setContents] = useState<Block<any>[]>([]);
   const [isPrinting, setIsPrinting] = useState(false);
   const theme = useTheme();
@@ -31,31 +45,76 @@ export default function Home() {
     }
   }
 
-  const initialContent = globalThis?.localStorage?.getItem("editorContent");
+  const initialContent = globalThis?.localStorage?.getItem(
+    `tab-${currentTabIndex}`
+  );
 
-  const editor = useBlockNote({
-    theme,
-    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
-    onEditorContentChange: (editor) => {
-      setContents(editor.topLevelBlocks);
-      globalThis?.localStorage?.setItem(
-        "editorContent",
-        JSON.stringify(editor.topLevelBlocks)
-      );
+  const editor = useBlockNote(
+    {
+      theme,
+      initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+      onEditorContentChange: (editor) => {
+        setContents(editor.topLevelBlocks);
+        globalThis?.localStorage?.setItem(
+          `tab-${currentTabIndex}` ?? "tab-0",
+          JSON.stringify(editor.topLevelBlocks)
+        );
+      },
+      slashCommands: [...defaultReactSlashMenuItems, ...Blocks],
+      blockSchema: {
+        ...defaultBlockSchema,
+        line: LineBlock,
+      },
+      customElements: {
+        dragHandleMenu: CustomDragHandleMenu as any,
+        formattingToolbar: CustomFormattingToolbar as any,
+      },
     },
-    slashCommands: [...defaultReactSlashMenuItems, ...Blocks],
-    blockSchema: {
-      ...defaultBlockSchema,
-      line: LineBlock,
-    },
-    customElements: {
-      dragHandleMenu: CustomDragHandleMenu as any,
-      formattingToolbar: CustomFormattingToolbar as any,
-    },
-  });
+    [currentTabIndex]
+  );
 
   return (
-    <main className="p-4 font-mono main">
+    <main className="p-4 font-mono main px-[54px]">
+      <div className="tabs w-full">
+        {tabs.map(({ title }, index) => (
+          <div
+            role="button"
+            key={index}
+            onClick={setTab(index)}
+            className={[
+              "tab tab-bordered flex-1 justify-between",
+              isCurrentTab(index) ? "tab-active" : "",
+            ].join(" ")}
+          >
+            <span>{title || "무제"}</span>
+
+            {tabs.length > 1 && (
+              <button
+                className="btn btn-square btn-xs btn-ghost"
+                onClick={removeTab(index)}
+              >
+                <FaXmark />
+              </button>
+            )}
+          </div>
+        ))}
+        <button
+          className="tab btn btn-square btn-sm btn-ghost ml-2"
+          onClick={newTab}
+        >
+          <FaPlus />
+        </button>
+      </div>
+
+      <input
+        type="text"
+        defaultValue={currentTab.title}
+        placeholder="제목을 입력해주세요"
+        value={currentTab.title}
+        onChange={renameTab(currentTabIndex)}
+        className="input input-ghost w-full my-4 p-0 rounded-sm text-xl font-bold"
+      />
+
       <BlockNoteView editor={editor} />
 
       <div className="flex justify-end mt-8">
